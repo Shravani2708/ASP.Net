69494987c9602cfe9564f34310a4444be185f380
Shravani2708 <39553939+Shravani2708@users.noreply.github.com>
2021-07-12 17:20:58 -0700

    Vaccine Managment system using EF core

---
+++ AppDBContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFIntroDemo
+{
+    class AppDBContext : DbContext
+    {
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            optionsBuilder
+               
+                .UseSqlServer(config.GetConnectionString("Default"))
+                .UseLazyLoadingProxies();
+        }
+       
+       
+        public DbSet<Records> Vaccine { get; set; }
+        
+    }
+}
---
+++ Records.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFIntroDemo
+{
+    
+    public class Records
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DosesRequired { get; set; }
+        public int Doses_between_Days { get; set; }
+        public int Doses_Recieved { get; set; }
+
+        public override string ToString()
+        {
+           return $"{Id}){Name}\t\t{DosesRequired}\t\t{Doses_between_Days}\t{Doses_Recieved}";
+        }
+
+
+
+    }
+}
+
